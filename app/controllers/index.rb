4e67453b70b5c70b5e8cbc8a63b8fd130c4a9002
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 09:41:32 -0500

    created db

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,18 @@
 get '/' do
   # Look in app/views/index.erb
+  @shortened_links = Url.all
   erb :index
 end
+
+post '/urls' do
+  # create a new Url
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  # redirect to appropriate "long" URL
+end
+
+
+
+
